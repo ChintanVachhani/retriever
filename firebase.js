6deb954d29f6c847f047b28ba821292a6dfc84f8
Chintan Vachhani <chintan.vachhani10@gmail.com>
2018-03-11 07:23:50 -0700

    Changed config.

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // firebase admin setup
 const firebaseAdmin = require('firebase-admin');
-const serviceAccount = require('./' + process.env.SERVICE_ACCOUNT_KEY);
+const serviceAccount = require(path.join(__dirname, process.env.SERVICE_ACCOUNT_KEY));
 firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert(serviceAccount),
     databaseURL: process.env.DATABASE_URL
